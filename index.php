7041f076561cfbea105de70eaa8757d5eb708048
Thoriq Ibadurrohman <thoriq312@gmail.com>
2021-10-10 13:37:21 +0700

    add chart title

--- index.php
+++ index.php
@@ -13,8 +13,13 @@
   <body>
       <div class="container">
           <h1>Dashboard Statistik Distribusi & Jasa</h1>
-        
+
+
+          <h2>Grafik Ekspor Impor Bulanan Provinsi Bali (US $)</h2>
           <div class="flourish-embed flourish-bar-chart-race" data-src="visualisation/7487567"><script src="https://public.flourish.studio/resources/embed.js"></script></div>
+          
+          <hr>
+          <h2>Nilai Ekspor Provinsi Bali Menurut Kawasan Tujuan (Ribu US$)</h2>
           <div class="flourish-embed flourish-bar-chart-race" data-src="visualisation/7451468"><script src="https://public.flourish.studio/resources/embed.js"></script></div>
       </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
